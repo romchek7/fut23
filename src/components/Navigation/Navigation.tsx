96a597b2ae95eeaa0ecb6a293bca8565701733e3
romchek7 <sonecccbbbaaa@gmail.com>
2022-10-25 10:45:06 +0300

    Create responsive burger menu

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import styles from "./Navigation.module.css"
+import {MenuOutlined, CloseOutlined} from '@ant-design/icons';
 
 interface ILinkNavigation {
     toPath: string
@@ -18,17 +19,24 @@ const LinkNavigation = (props: ILinkNavigation) => {
 }
 
 const Navigation: React.FC = () => {
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
-        <nav className={styles.nav}>
-            <LinkNavigation toPath={'/'} text='Home'/>
-            <LinkNavigation toPath={'/leagues'} text='Leagues'/>
-            <LinkNavigation toPath={'/teams'} text='Teams'/>
-            <LinkNavigation toPath={'/players'} text='Players'/>
-            <LinkNavigation toPath={'/bookmakers'} text='Bookmakers'/>
-            <LinkNavigation toPath={'/markets'} text='Markets'/>
-            <LinkNavigation toPath={'/referees'} text='Referees'/>
-            <LinkNavigation toPath={'/venues'} text='Venues'/>
-        </nav>
+        <>
+            <div className={styles.menuBtn} onClick={() => setShowMenu(prevState => !prevState)}>
+                {showMenu ? <CloseOutlined /> : <MenuOutlined />}
+            </div>
+            <nav className={showMenu ? styles.navMobile : styles.nav}>
+                <LinkNavigation toPath={'/'} text='Home'/>
+                <LinkNavigation toPath={'/leagues'} text='Leagues'/>
+                <LinkNavigation toPath={'/teams'} text='Teams'/>
+                <LinkNavigation toPath={'/players'} text='Players'/>
+                <LinkNavigation toPath={'/bookmakers'} text='Bookmakers'/>
+                <LinkNavigation toPath={'/markets'} text='Markets'/>
+                <LinkNavigation toPath={'/referees'} text='Referees'/>
+                <LinkNavigation toPath={'/venues'} text='Venues'/>
+            </nav>
+        </>
     )
 }
 
