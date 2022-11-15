fb08e85aa506a472f24c00ceb6a3203dd07a6baf
romchek7 <sonecccbbbaaa@gmail.com>
2022-11-15 13:11:43 +0200

    Replace BrowserRouter to HashRouter only for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-	<BrowserRouter>
+	<HashRouter>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
 )
 
 reportWebVitals()
